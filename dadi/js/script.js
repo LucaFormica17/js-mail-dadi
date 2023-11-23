e0898f8654f7f0b3145fa3e036c9451fd326f3e7
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2023-11-23 19:24:46 +0100

    [UPLOAD] bonus

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,18 +1,26 @@
 //generazione random per utente
 let user = Math.floor((Math.random() * 6) + 1);
 console.log(user);
+let view_user = document.getElementById('user');
+let h3_user = `<h3>${user}</h3>`;
+view_user.innerHTML += h3_user;
 
 //generazione random per cpu
 let cpu = Math.floor((Math.random() * 6) + 1);
 console.log(cpu);
+let view_cpu = document.getElementById('cpu');
+h3_cpu = `<h3>${cpu}</h3>`;
+view_cpu.innerHTML += h3_cpu;
 
+let view_text = document.getElementById('text');
 //confronta risultati ottenuti e dichiara se hai vinto o hai perso
 if(user > cpu){
-    console.log('Hai vinto!');
+    view_text.innerText = ('Hai vinto!');
+    
 }
 else if(user < cpu){
-    console.log('Hai perso!');
+    view_text.innerText = ('Hai perso!');
 }
 else{
-    console.log('Pareggio!');
+    view_text.innerText = ('Pareggio!');
 }
--- mail/js/script.js
+++ mail/js/script.js
@@ -4,11 +4,13 @@ let mail = prompt('Inserisci Mail');
 //creare array contenente mail di controllo
 const notValidMail = ['pippo@gmail.com', 'pluto@gmail.com', 'caio@gmail.com', 'tizio@gmail.com', 'sempronio@gmail'];
 
+let view_title = document.getElementById('title');
+
 for(let i=0; i<notValidMail.length; i++){
     if(notValidMail[i] == mail){
         alert("Mail già usata! inserisci un'altra Mail");
     }
     else{
-        console.log('Mail valida!');
+        view_title.innerText = ('Benvenuto!')
     }
 }
